ef71ee79bad9bcf0e6797429953da75a80bd9d14
Saint-es <Callum06660@gmail.com>
2026-02-28 13:57:48 +0000

    Update investigstion.lua

--- client/investigstion.lua
+++ client/investigstion.lua
@@ -23,3 +23,25 @@ RegisterNetEvent("fire:server:createReport", function(fireId, cause, notes)
     })
 
 end)
+
+RegisterCommand("firereport", function()
+
+    local input = lib.inputDialog("Fire Investigation Report", {
+        { type = "input", label = "Fire ID", required = true },
+        { type = "select", label = "Cause", options = {
+            { label = "Accidental", value = "Accidental" },
+            { label = "Electrical Fault", value = "Electrical" },
+            { label = "Arson", value = "Arson" }
+        }},
+        { type = "textarea", label = "Notes", required = true }
+    })
+
+    if not input then return end
+
+    TriggerServerEvent("fire:server:createReport",
+        input[1],
+        input[2],
+        input[3]
+    )
+
+end)
