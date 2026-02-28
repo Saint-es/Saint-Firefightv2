07452f7e30754a5f7258ddee5c24c5a894e21592
Saint-es <Callum06660@gmail.com>
2026-02-28 13:43:04 +0000

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,58 @@
+Config = {}
+
+-- Framework Options
+Config.Framework = {
+    FireJobName = "fire"
+}
+
+-- Inventory Item Names
+Config.Items = {
+    HazmatSuit = "hazmat_suit",
+    SCBA = "scba_tank",
+    RescueCushion = "rescue_cushion"
+}
+
+-- Department
+Config.Department = {
+    StartingBudget = 100000
+}
+
+-- Rescue Cushion
+Config.RescueCushion = {
+    AuthorizedVehicles = {
+        ['firetruk'] = true,
+        ['firetruk2'] = true,
+        ['laddertruck'] = true
+    },
+    InflateTime = 5000,
+    Radius = 4.5,
+    SafeHeight = 8.0,
+    DamageReduction = 0.9
+}
+
+-- Fire Types
+Config.FireTypes = {
+    Chemical = {
+        requiredMethod = "foam",
+        waterMakesWorse = true,
+        spreadRate = 1.5
+    },
+    Bin = {
+        requiredMethod = "water",
+        spreadRate = 0.3
+    },
+    House = {
+        requiredMethod = "water",
+        spreadRate = 1.0
+    },
+    Shop = {
+        requiredMethod = "water",
+        requiresSCBA = true,
+        spreadRate = 1.2
+    },
+    Wild = {
+        requiredMethod = "water",
+        windAmplified = true,
+        spreadRate = 2.0
+    }
+}
