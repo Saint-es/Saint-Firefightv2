0da89be5c11ccf61bd43554950f9106143b1e69c
Saint-es <Callum06660@gmail.com>
2026-02-28 14:02:43 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -16,3 +16,16 @@ end)
 RegisterNetEvent("fire:client:fireResolved", function(fireId)
     ClientFires[fireId] = nil
 end)
+
+RegisterNetEvent("fire:server:getIncidents", function()
+    local src = source
+
+    local results = MySQL.query.await("SELECT * FROM fire_incidents ORDER BY created_at DESC LIMIT 10")
+
+    TriggerClientEvent("fire:client:showIncidents", src, results)
+end)
+
+RegisterNetEvent("fire:server:getBudget", function()
+    local src = source
+    TriggerClientEvent("fire:client:showBudget", src, FireDepartment.budget)
+end)
