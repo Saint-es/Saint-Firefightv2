633d779f45e303c82cd861b4c7efa656dc7c60da
Saint-es <Callum06660@gmail.com>
2026-02-28 14:02:10 +0000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -17,3 +17,16 @@ CreateThread(function()
         )
     end
 end)
+
+RegisterNetEvent("fire:server:getIncidents", function()
+    local src = source
+
+    local results = MySQL.query.await("SELECT * FROM fire_incidents ORDER BY created_at DESC LIMIT 10")
+
+    TriggerClientEvent("fire:client:showIncidents", src, results)
+end)
+
+RegisterNetEvent("fire:server:getBudget", function()
+    local src = source
+    TriggerClientEvent("fire:client:showBudget", src, FireDepartment.budget)
+end)
